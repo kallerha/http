f8fcf17896f241e634cf9fe1c828ef469b7b6bc4
Kappit <kasper.h90@outlook.dk>
2021-09-29 14:41:07 +0200

    no message

--- src/Messages/Response/StatusCodes.php
+++ src/Messages/Response/StatusCodes.php
@@ -14,6 +14,7 @@ class StatusCodes
     public const OK = 200;
     public const FOUND = 302;
     public const SEE_OTHER = 303;
+    public const UNAUTHORIZED = 401;
     public const FORBIDDEN = 403;
     public const NOT_FOUND = 404;
     public const METHOD_NOT_ALLOWED = 405;
