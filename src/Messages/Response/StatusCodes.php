55578ec69e8aa6501cffd83d48ab4aeeb1cd1b8b
Kappit <kasper.h90@outlook.dk>
2021-10-04 08:14:55 +0200

    no message

--- src/Messages/Response/StatusCodes.php
+++ src/Messages/Response/StatusCodes.php
@@ -14,6 +14,7 @@ class StatusCodes
     public const OK = 200;
     public const FOUND = 302;
     public const SEE_OTHER = 303;
+    public const BAD_REQUEST = 400;
     public const UNAUTHORIZED = 401;
     public const FORBIDDEN = 403;
     public const NOT_FOUND = 404;
