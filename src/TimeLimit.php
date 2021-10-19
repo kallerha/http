bb9abdac88db141512038ec81dd8c6e764626bf4
Kappit <kasper.h90@outlook.dk>
2021-10-19 12:41:30 +0200

    no message

--- src/TimeLimit.php
+++ src/TimeLimit.php
@@ -14,10 +14,11 @@ class TimeLimit
     public function __construct(int $seconds)
     {
 
-        register_shutdown_function(function () {
+        register_shutdown_function(function (): void {
             if ($error = error_get_last()) {
                 if (str_contains($error['message'], 'Maximum execution time')) {
                     http_response_code(StatusCodes::GATEWAY_TIMEOUT);
+
                     exit;
                 }
             }
